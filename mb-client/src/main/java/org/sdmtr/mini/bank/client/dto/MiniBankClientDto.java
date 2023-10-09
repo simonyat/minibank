8686476ebe49aa3799462ca7a58b69e204f50f01
Симонятов Дмитрий <aleksandr.a.kotov@gazprombank.ru>
2023-10-09 23:27:09 +0300

    Добавлен класс DTO клиента мини банка

---
+++ mb-client/src/main/java/org/sdmtr/mini/bank/client/dto/MiniBankClientDto.java
@@ -0,0 +1,21 @@
+package org.sdmtr.mini.bank.client.dto;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @param userName
+ * @param firstName
+ * @param surName
+ * @param patronymicName
+ * @param email
+ * @param password
+ */
+public record MiniBankClientDto(String userName,
+                                String firstName,
+                                String surName,
+                                String patronymicName,
+                                String email,
+                                String password) implements Serializable {
+
+}
