f2ff7cd3f0907e57dbcc25f222f73a480086e1e7
Симонятов Дмитрий <aleksandr.a.kotov@gazprombank.ru>
2023-10-12 23:55:26 +0300

    Добавлены зависимости для Spring Boot

--- mb-client/build.gradle.kts
+++ mb-client/build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
     id("org.sdmtr.mini.bank.java-application-conventions")
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
 }
 
 dependencies {
     implementation(project(":mb-common"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 application {
-//    mainClass.set("org.sdmtr.mini.bank.app.App")
+    mainClass.set("org.sdmtr.mini.bank.client.MiniBankClientApplication")
 }
---
+++ mb-client/src/main/java/org/sdmtr/mini/bank/client/MiniBankClientApplication.java
@@ -0,0 +1,13 @@
+package org.sdmtr.mini.bank.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBankClientApplication {
+
+    public static void main(String[] argv) {
+        SpringApplication.run(MiniBankClientApplication.class, argv);
+    }
+
+}
