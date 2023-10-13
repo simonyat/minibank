f7f290d21db048cbfed0ce459c28e0e86349691f
Симонятов Дмитрий <aleksandr.a.kotov@gazprombank.ru>
2023-10-13 16:06:03 +0300

    Добавлен контроллер для работы с клиентами

--- mb-client/src/main/java/org/sdmtr/mini/bank/client/controller/ClientController.java
+++ mb-client/src/main/java/org/sdmtr/mini/bank/client/controller/ClientController.java
@@ -3,17 +3,23 @@ package org.sdmtr.mini.bank.client.controller;
 import org.sdmtr.mini.bank.client.dto.MiniBankClientDto;
 import org.sdmtr.mini.bank.model.web.request.RequestItem;
 import org.sdmtr.mini.bank.model.web.response.ResponseItem;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/minibank")
+@RequestMapping("/minibank/client")
 public class ClientController {
 
-    public ResponseItem<MiniBankClientDto> create(RequestItem<MiniBankClientDto> newClient) {
+    @PostMapping("/create")
+    public ResponseItem<MiniBankClientDto> create(@RequestBody RequestItem<MiniBankClientDto> newClient) {
+
+
 
         ResponseItem<MiniBankClientDto> result =
                 new ResponseItem<>("success", newClient.data(), null);
+
 
         return result;
     }
